9a9900ec5a4a025f20a4766b66ec84f34c0588a1
TataynaSmychkova <Mella2005@rambler.ru>
2023-03-16 23:39:00 +0700

    Модифицировала конструктор Channel, добавила класс-метод get_service() и метод to_json()

--- src/channel.py
+++ src/channel.py
@@ -9,10 +9,27 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.__channel_id = channel_id
+        self.__youtube = Channel.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.__youtube['items'][0]['snippet']['title']
+        self.description = self.__youtube['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.quantity_subscribers = self.__youtube['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.__youtube['items'][0]['statistics']['videoCount']
+        self.quantity_views = self.__youtube['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.__youtube, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def to_json(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.__dict__, file, indent=2)
